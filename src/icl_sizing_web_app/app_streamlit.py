33bb09ddfae2ffd5f40014f7bd41775c12b23a36
Sebastian Ortmanns <sebastian.ortmanns@inovex.de>
2024-02-03 10:55:58 +0100

    test

--- src/icl_sizing_web_app/app_streamlit.py
+++ src/icl_sizing_web_app/app_streamlit.py
@@ -10,8 +10,13 @@ from snowflake.snowpark import functions as F
 from snowflake.snowpark.session import _get_active_sessions
 from snowflake.snowpark.types import (DoubleType, IntegerType, StringType,
                                       StructField, StructType)
+from propelauth_py import UnauthorizedException, init_base_auth
+from streamlit.web.server.websocket_headers import _get_websocket_headers
 
-from icl_sizing_web_app.propelauth import auth
+auth = Auth(
+    "https://307943155.propelauthtest.com",
+    "a1736840363c61fd07853cc7898f849b55f9eb18b573a3b6c9a265752476494d528c814849d34adaca66b7f1df22a654",
+)
 
 user = auth.get_user()
 if user is None:
@@ -186,3 +191,40 @@ st.text("Logged in as " + user.email + " with user ID " + user.user_id)
 #     st.error('Username/password is incorrect')
 # elif authentication_status == None:
 #     st.warning('Please enter your username and password')
+
+
+class Auth:
+    def __init__(self, auth_url, integration_api_key):
+        self.auth = init_base_auth(auth_url, integration_api_key)
+        self.auth_url = auth_url
+
+    def get_user(self):
+        access_token = get_access_token()
+
+        if not access_token:
+            return None
+
+        try:
+            return self.auth.validate_access_token_and_get_user(
+                "Bearer " + access_token
+            )
+        except UnauthorizedException as err:
+            print("Error validating access token", err)
+            return None
+
+    def get_account_url(self):
+        return self.auth_url + "/account"
+
+
+def get_access_token():
+    headers = _get_websocket_headers()
+    if headers is None:
+        return None
+
+    cookies = headers.get("Cookie") or headers.get("cookie") or ""
+    for cookie in cookies.split(";"):
+        split_cookie = cookie.split("=")
+        if len(split_cookie) == 2 and split_cookie[0].strip() == "__pa_at":
+            return split_cookie[1].strip()
+
+    return None
